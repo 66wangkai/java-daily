5393460624a7f1aac0890c849aa6aa3ffbeb3bac
Wangkai <483348772@qq.com>
2020-10-28 17:38:06 +0800

    first commit

---
+++ everycode/src/com/wk/java/day1/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.wk.java.day1;
+
+/**
+ * @ClassName HelloWorld
+ * @Description TODO
+ * @Author wangkai
+ * @Date 2020/10/28
+ **/
+public class HelloWorld {
+    public static void main (String [] args){
+        System.out.println("HelloWorld!!");
+    }
+
+}
