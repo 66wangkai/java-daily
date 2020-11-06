f24570e3322362495824142f1aef008ab311204b
Wangkai <483348772@qq.com>
2020-11-06 22:44:36 +0800

    day17

---
+++ everycode/src/com/wk/java/day17/Example2_7.java
@@ -0,0 +1,29 @@
+package com.wk.java.day17;
+
+import java.security.PublicKey;
+import java.util.Arrays;
+
+/**
+ * @ClassName Example2_7
+ * @Description TODO
+ * @Author wangkai
+ * @Date 2020/11/6
+ **/
+public class Example2_7 {
+    public static void main(String args[]) {
+        int [] a = {12,34,9,23,45,6,45,90,123,19,34};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        int number = 45;
+        int index = Arrays.binarySearch(a,number);
+        if(index>=0){
+            System.out.println(number + "和数组中索引为" + index + "的元素相同");
+
+        }
+        else{
+            System.out.println(number + "不与数组中的任何元素值相同");
+
+        }
+    }
+
+}
