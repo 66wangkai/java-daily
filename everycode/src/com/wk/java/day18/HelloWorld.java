1c52c0b10896e052c974b95fcce4538c2f82433f
Wangkai <483348772@qq.com>
2020-11-08 21:58:51 +0800

    day19

---
+++ everycode/src/com/wk/java/day18/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.wk.java.day18;
+
+/**
+ * @ClassName HelloWorld
+ * @Description TODO
+ * @Author wangkai
+ * @Date 2020/11/8
+ **/
+
+    public class HelloWorld {
+        public static void main(String[] args) {
+            int year = 1949;
+        }
+    }
+
